9af0a66bbc7561fad05ed4889d42f0f5a0aba69f
mustafatavasli <musstafatavasli@gmail.com>
2024-04-07 12:41:39 +0300

    Initial Commit

---
+++ ParaUstu/ParaUstuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParaUstuApp.swift
+//  ParaUstu
+//
+//  Created by Mustafa TAVASLI on 7.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ParaUstuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
